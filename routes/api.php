9d6b6afceea27a05dd4f5fd4c3be8f41cec4c46d
NVaissaud <n.vaissaud@gmail.com>
2020-03-19 20:00:49 +0100

    Profile participations

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,8 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('user', 'Api\UserController@show');
 
     Route::post('profile/{profile}', 'Api\ProfileController@update');
+    Route::post('profile/{profile}/participations', 'Api\ProfileController@participations');
+
     Route::post('structure', 'Api\StructureController@store');
     Route::post('structure/{structure}', 'Api\StructureController@update');
 
