e42491d64fc5e2012fa0346142e7718f9b22cdd0
Jeremy <jeremy@codeconut.fr>
2020-03-20 10:11:10 +0100

    Userback

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,5 +9,14 @@
     <body>
         <div id="app"></div>
         <script type="text/javascript" src="{{ mix('/assets/js/app.js') }}"></script>
+        <script>
+            Userback = window.Userback || {};
+            Userback.access_token = '8654|15306|z7JRFuJkExuIVFcztrli1HqKOnGVSikUhotmIJl6M1x3tJKtaW';
+            (function(id) {
+                var s = document.createElement('script');
+                s.async = 1;s.src = 'https://static.userback.io/widget/v1.js';
+                var parent_node = document.head || document.body;parent_node.appendChild(s);
+            })('userback-sdk');
+        </script>
     </body>
 </html>
