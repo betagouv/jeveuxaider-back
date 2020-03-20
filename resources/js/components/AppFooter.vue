8aaabcabcf9c8c742215cfcd01af09af38dfaeab
Ijahman <kevin.vaissaud@gmail.com>
2020-03-20 16:23:08 +0100

    page FAQ

--- resources/js/components/AppFooter.vue
+++ resources/js/components/AppFooter.vue
@@ -1,58 +1,143 @@
 <template>
   <footer class="bg-blue-royal text-white text-center lg:text-left">
     <div class="container mx-auto px-4 py-8">
-        <div class="flex flex-col lg:flex-row items-center lg:items-start">
-            <div class="max-w-xs">
-                <div class="mb-8">
-                    <router-link to="/">
-                        <img class="mx-auto lg:mx-0" src="/images/logo-reserve-civique_light.svg" alt="" style="height: 30px;" />
-                    </router-link>
-                </div>
-                <div class="text-white" style="max-width:260px; font-size:14px; line-height: 1.6; letter-spacing: 0.05em;">Trouvez une mission d'intérêt général auprès d'un organisme public ou associatif et faites vivre les valeurs de la République.</div>
-            </div>
-            <div id="menu-footer" class="lg:flex lg:flex-1 lg:justify-around mt-8 lg:mt-0">
-                <div class="lg:max-w-1/4 text-white">
-                    <router-link class="block p-3 hover:underline" to="/a-propos">A propos</router-link>
-                    <router-link class="block p-3 hover:underline" to="/regles-de-securite">Règles de sécurité</router-link>
-                </div>
-                <div class="lg:max-w-1/4 text-white">
-                    <a class="block p-3 hover:underline" href="https://www.elysee.fr/" target="_blank">elysee.fr</a>
-                    <a class="block p-3 hover:underline" href="https://service-public.fr" target="_blank">service-public.fr</a>
-                    <a class="block p-3 hover:underline" href="https://legifrance.gouv.fr" target="_blank">legifrance.gouv.fr</a>
-                    <a class="block p-3 hover:underline" href="https://data.gouv.fr" target="_blank">data.gouv.fr</a>
-                    <a class="block p-3 hover:underline" href="https://france.fr" target="_blank">france.fr</a>
-                </div>
-                <div class="lg:max-w-1/2 text-white">
-                    <a class="block p-3 hover:underline" href="https://www.service-civique.gouv.fr/" target="_blank">Service Civique</a>
-                    <a class="block p-3 hover:underline" href="https://www.snu.gouv.fr/" target="_blank">Service National Universel</a>
-                    <a class="block p-3 hover:underline" href="https://www.corpseuropeensolidarite.fr/" target="_blank">Corps Européen de solidarité</a>
-                    <a class="block p-3 hover:underline" href="https://www.service-public.fr/particuliers/vosdroits/F11444" target="_blank">Volontariat de solidarité internationale</a>
-                    <a class="block p-3 hover:underline" href="https://garde-nationale.gouv.fr/" target="_blank">Garde Nationale</a>
-                </div>
-            </div>
-            <div class="mt-8 lg:mt-3">
-                <div class="mb-8" style="font-size:13px;">Suivez la Réserve Civique</div>
-                <div class="flex -m-2 overflow-hidden justify-center lg:justify-start">
-                    <a class="m-2 flex-shrink-0 flex items-center justify-center border rounded-full w-10 h-10 transition opacity-75 hover:opacity-100" href="https://twitter.com/gouvernementfr" target="_blank">
-                        <img src="/images/twitter_white.svg" alt="Twitter" />
-                    </a>
-                    <a class="m-2 flex-shrink-0 flex items-center justify-center border rounded-full w-10 h-10 transition opacity-75 hover:opacity-100" href="https://www.facebook.com/gouvernement.fr" target="_blank">
-                        <img src="/images/facebook_white.svg" alt="Facebook" />
-                    </a>
-                </div>
-            </div>
+      <div class="flex flex-col lg:flex-row items-center lg:items-start">
+        <div class="max-w-xs">
+          <div class="mb-8">
+            <router-link to="/">
+              <img
+                class="mx-auto lg:mx-0"
+                src="/images/logo-reserve-civique_light.svg"
+                alt=""
+                style="height: 30px;"
+              />
+            </router-link>
+          </div>
+          <div
+            class="text-white"
+            style="max-width:260px; font-size:14px; line-height: 1.6; letter-spacing: 0.05em;"
+          >
+            Trouvez une mission d'intérêt général auprès d'un organisme public
+            ou associatif et faites vivre les valeurs de la République.
+          </div>
         </div>
+        <div
+          id="menu-footer"
+          class="lg:flex lg:flex-1 lg:justify-around mt-8 lg:mt-0"
+        >
+          <div class="lg:max-w-1/4 text-white">
+            <router-link class="block p-3 hover:underline" to="/a-propos"
+              >A propos</router-link
+            >
+            <router-link
+              class="block p-3 hover:underline"
+              to="/regles-de-securite"
+              >Règles de sécurité</router-link
+            >
+            <router-link class="block p-3 hover:underline" to="/faq"
+              >FAQ</router-link
+            >
+          </div>
+          <div class="lg:max-w-1/4 text-white">
+            <a
+              class="block p-3 hover:underline"
+              href="https://www.elysee.fr/"
+              target="_blank"
+              >elysee.fr</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://service-public.fr"
+              target="_blank"
+              >service-public.fr</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://legifrance.gouv.fr"
+              target="_blank"
+              >legifrance.gouv.fr</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://data.gouv.fr"
+              target="_blank"
+              >data.gouv.fr</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://france.fr"
+              target="_blank"
+              >france.fr</a
+            >
+          </div>
+          <div class="lg:max-w-1/2 text-white">
+            <a
+              class="block p-3 hover:underline"
+              href="https://www.service-civique.gouv.fr/"
+              target="_blank"
+              >Service Civique</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://www.snu.gouv.fr/"
+              target="_blank"
+              >Service National Universel</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://www.corpseuropeensolidarite.fr/"
+              target="_blank"
+              >Corps Européen de solidarité</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://www.service-public.fr/particuliers/vosdroits/F11444"
+              target="_blank"
+              >Volontariat de solidarité internationale</a
+            >
+            <a
+              class="block p-3 hover:underline"
+              href="https://garde-nationale.gouv.fr/"
+              target="_blank"
+              >Garde Nationale</a
+            >
+          </div>
+        </div>
+        <div class="mt-8 lg:mt-3">
+          <div class="mb-8" style="font-size:13px;">
+            Suivez la Réserve Civique
+          </div>
+          <div
+            class="flex -m-2 overflow-hidden justify-center lg:justify-start"
+          >
+            <a
+              class="m-2 flex-shrink-0 flex items-center justify-center border rounded-full w-10 h-10 transition opacity-75 hover:opacity-100"
+              href="https://twitter.com/gouvernementfr"
+              target="_blank"
+            >
+              <img src="/images/twitter_white.svg" alt="Twitter" />
+            </a>
+            <a
+              class="m-2 flex-shrink-0 flex items-center justify-center border rounded-full w-10 h-10 transition opacity-75 hover:opacity-100"
+              href="https://www.facebook.com/gouvernement.fr"
+              target="_blank"
+            >
+              <img src="/images/facebook_white.svg" alt="Facebook" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    <hr class="border-blue-800 mt-4">
+    <hr class="border-blue-800 mt-4" />
     <div class="container mx-auto px-4 py-6">
-        <div style="font-size: 10px; letter-spacing: 0.05em;">© RESERVE-CIVIQUE.GOUV.FR, 2020</div>
+      <div style="font-size: 10px; letter-spacing: 0.05em;">
+        © RESERVE-CIVIQUE.GOUV.FR, 2020
+      </div>
     </div>
-</footer>
-
+  </footer>
 </template>
 
 <script>
-
 export default {
   name: "AppFooter",
   methods: {
