448d840e2d532e1584d6fd35179f74675ac0cb91
Ijahman <kevin.vaissaud@gmail.com>
2020-03-30 19:39:16 +0200

    factory user, profile, structure

--- database/factories/StructureFactory.php
+++ database/factories/StructureFactory.php
@@ -4,14 +4,44 @@
 
 use Faker\Generator as Faker;
 use App\Models\Structure;
+use Illuminate\Support\Facades\Event;
+
+// To prevent emails from being sent,
+// otherwise get error -> too many emails per second
+Event::fake();
 
 $factory->define(Structure::class, function (Faker $faker) {
-    $statuts = config('taxonomies.structure_legal_status.terms');
+    $profile = factory(App\Models\Profile::class)->create();
+    $statuts = array_rand(config('taxonomies.structure_legal_status.terms'));
+    $associationType = $statuts == 'Association' ? array_rand(config('taxonomies.association_types.terms')) : null;
+    $organizationPublicType = $statuts == 'Structure publique' ? array_rand(config('taxonomies.structure_publique_types.terms')) : null;
+    $organizationPrivateType = $statuts == 'Structure privée' ? array_rand(config('taxonomies.structure_privee_types.terms')) : null;
+    $isReseau = $faker->boolean();
+
 
     return [
-        'name' => $faker->name,
-        'description' => $faker->sentence(4),
-        'statut_juridique' => $statuts['Association'],
-        'is_reseau' => 1
+        'user_id' => $profile->user->id,
+        'name' => $faker->company,
+        'description' => $faker->sentence(40),
+        'statut_juridique' => $statuts,
+        'is_reseau' => $isReseau,
+        'reseau_id' => $isReseau ? Structure::inRandomOrder()->first()->id : null,
+        'siret' => $faker->siret,
+        'structure_publique_type' => $organizationPublicType,
+        'structure_privee_type' => $organizationPrivateType,
+        'association_types' => $associationType,
+        'address' => $faker->streetAddress,
+        'zip' => $faker->postcode,
+        'city' => $faker->city,
+        'country' => $faker->country,
+        'department' => $faker->departmentNumber,
+        'latitude' => $faker->latitude,
+        'longitude' => $faker->longitude
     ];
+});
+
+$factory->afterCreating(Structure::class, function ($structure, $faker) {
+    $structure->members()->attach($structure->user->profile, ['role' => 'responsable']);
+
+    // TODO: créér automatiquement quelques missions.
 });
