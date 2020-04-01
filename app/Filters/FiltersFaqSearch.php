7827ecf4d51ecb6a7d72d7be833999a38fbce814
NVaissaud <n.vaissaud@gmail.com>
2020-04-01 18:40:14 +0200

    FiltreFaqSearch

---
+++ app/Filters/FiltersFaqSearch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filters;
+
+use Spatie\QueryBuilder\Filters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+class FiltersFaqSearch implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property): Builder
+    {
+        return $query
+            ->where('title', 'LIKE', '%' . $value . '%')
+            ->orWhere('description', 'LIKE', '%' . $value . '%')
+        ;
+    }
+}
