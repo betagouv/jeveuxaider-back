a958627723836ffff2d041800d4b6a3c1366a479
Jeremy <jeremy@codeconut.fr>
2020-04-15 18:33:23 +0200

    Clean Requests

--- app/Http/Requests/Api/ReleaseUpdateRequest.php
+++ app/Http/Requests/Api/ReleaseUpdateRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests\Api;
 
-use App\Http\Requests\ReleaseRequest;
-
-class ReleaseUpdateRequest extends ReleaseRequest
+class ReleaseUpdateRequest
 {
     /**
      * Determine if the user is authorized to make this request.
