785459226f09dc3c3803f545e3a68d43c106ac48
NVaissaud <n.vaissaud@gmail.com>
2020-04-17 16:34:41 +0200

    Recettage

--- resources/js/views/Front/UserMissions.vue
+++ resources/js/views/Front/UserMissions.vue
@@ -135,23 +135,6 @@
                 </span>
               </div>
 
-              <button
-                v-if="participation.mission.tuteur.mobile"
-                type="button"
-                class="m-2 relative shadow-sm inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white"
-              >
-                <svg
-                  class="-ml-1 mr-2 h-5 w-5 text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
-                  />
-                </svg>
-                <span>{{ participation.mission.tuteur.mobile }}</span>
-              </button>
-
               <a
                 :href="
                   `mailto:${participation.mission.tuteur.email}`
