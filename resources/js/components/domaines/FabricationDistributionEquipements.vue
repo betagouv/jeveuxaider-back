bcc7b10f3f443bbb492c58158a8b954f802bae6b
NVaissaud <n.vaissaud@gmail.com>
2020-05-04 15:20:25 +0200

    New wording Fabrication masques

--- resources/js/components/domaines/FabricationDistributionEquipements.vue
+++ resources/js/components/domaines/FabricationDistributionEquipements.vue
@@ -10,13 +10,13 @@
       <div class="mt-8">
         <p
           class="ml-3 text-gray-700"
-        >Dans la perspective du déconfinement, l’usage de masques grand public doit se développer de manière massive au sein de la population générale. Cela suppose de produire ces équipements en grandes quantités.</p>
+        >Dans la perspective du déconfinement, l’usage de masques grand public est recommandé dans toutes les situations où la distanciation sociale ne peut être assurée.</p>
         <p
           class="ml-3 text-gray-700"
-        >Des associations, des entreprises adaptées ou des établissements et services d’aide par le travail (ESAT) se sont mobilisées et ont besoin de renfort pour produire plusieurs dizaines de milliers d’équipements de protection par semaine.</p>
+        >Des associations, des entreprises adaptées, des établissements et services d’aide par le travail (ESAT) se sont mobilisées et ont besoin de renfort pour produire plusieurs dizaines de milliers d’équipements de protection et de masques grand public par semaine.</p>
         <p
           class="ml-3 text-gray-700"
-        >La production de masques barrières doit obéir à un cahier des charges qui a donné lieu à des tests (<a target="_blank" href="https://www.entreprises.gouv.fr/covid-19/liste-des-tests-masques-de-protection">https://www.entreprises.gouv.fr/covid-19/liste-des-tests-masques-de-protection</a>). </p>
+        >La production de masques doit obéir à un cahier des charges validé, qui a donné lieu à des tests (<a target="_blank" href="https://www.entreprises.gouv.fr/covid-19/liste-des-tests-masques-de-protection">https://www.entreprises.gouv.fr/covid-19/liste-des-tests-masques-de-protection</a>), afin de garantir la protection maximale de chaque masque grand public. </p>
       </div>
     </div>
     <div class="mt-12">
@@ -29,16 +29,19 @@
       <div class="mt-8">
         <div class="ml-3 text-gray-700">
           <p>
-            Pour l’action de confection des masques barrières, j’utilise une machine à coudre, à partir d’un kit comprenant étoffe, élastiques et étiquette, en respectant strictement les exigences de contrôle qualité de la fabrication (<a target="_blank" href="https://masques-barrieres.afnor.org/home/faire-mon-masquebarriere">https://masques-barrieres.afnor.org/home/faire-mon-masquebarriere</a>). J’ai déjà utilisé une machine à coudre. Eventuellement, je peux mettre à disposition ma machine à coudre personnelle.
+            Je participe aux actions de confection des équipements de protection, notamment de masques grand public, ou à leur distribution.
+          </p>
+          <p>
+            Pour l’action de confection des masques, j’utilise une machine à coudre, à partir d’un kit comprenant étoffe, élastiques et étiquette, en respectant strictement les exigences de contrôle qualité de la fabrication (<a target="_blank" href="https://masques-barrieres.afnor.org/home/faire-mon-masquebarriere">https://masques-barrieres.afnor.org/home/faire-mon-masquebarriere</a>). J’ai déjà utilisé une machine à coudre. Eventuellement, je peux mettre à disposition ma machine à coudre personnelle.
           </p>
           <p>
             La mission de confection se déroule au sein d’un atelier de production ou à domicile.
           </p>
           <p>
-            Pour me rendre sur site, le cas échéant, je me munis de mon autorisation de déplacement et, autant que possible, je n’emprunte pas les transports en commun. Sur place, je respecte les règles de sécurité et les gestes barrières afin de me protéger et de protéger les autres. Au moindre doute d’infection, je ne me mobilise pas et je reste chez moi.
+            Pour me rendre sur site, le cas échéant, je me munis de mon autorisation de déplacement. Sur place, je respecte les règles de sécurité et les gestes barrières afin de me protéger et de protéger les autres. Au moindre doute d’infection, je ne me mobilise pas et je reste chez moi.
           </p>
           <p>
-            Cette mission n’est accessible qu’aux personnes majeures, de moins de 70 ans et en bonne santé.
+            Cette mission n’est accessible qu’aux personnes majeures.
           </p>
         </div>
       </div>
