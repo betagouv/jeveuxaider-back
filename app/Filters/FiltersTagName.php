c71e5401bb37032c3b399ca7c673473135d2afc8
NVaissaud <n.vaissaud@gmail.com>
2020-06-19 16:04:45 +0200

    FilterTagName

--- app/Filters/FiltersTagName.php
+++ app/Filters/FiltersTagName.php
@@ -10,8 +10,9 @@ class FiltersTagName implements Filter
     public function __invoke(Builder $query, $value, string $property): Builder
     {
         return $query->where(function ($query) use ($value, $property) {
-            // $query->whereRaw("json_unquote(json_extract(`name`, '$.\"fr\"')) ILIKE '%" . $value . "%') or `group` LIKE '%" . $value . "%'");
-            $query->whereRaw("unaccent(json_unquote(json_extract(`name`, '$.\"fr\"'))) ILIKE unaccent('%" . $value . "%') or `group` LIKE unaccent('%" . $value . "%')");
+            $query->where('name->fr', 'ILIKE', '%' . $value . '%')
+                ->orWhere('group', 'ILIKE', '%' . $value . '%')
+            ;
         });
     }
 }
