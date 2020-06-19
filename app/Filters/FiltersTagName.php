195e74bcf1cf1f0099dfc54d8f2c4516e4156cb6
NVaissaud <n.vaissaud@gmail.com>
2020-06-19 15:31:31 +0200

    ILIKE

--- app/Filters/FiltersTagName.php
+++ app/Filters/FiltersTagName.php
@@ -10,8 +10,8 @@ class FiltersTagName implements Filter
     public function __invoke(Builder $query, $value, string $property): Builder
     {
         return $query->where(function ($query) use ($value, $property) {
-            $query->where('name->fr', 'LIKE', '%' . $value . '%')
-                ->orWhere('group', 'LIKE', '%' . $value . '%')
+            $query->where('name->fr', 'ILIKE', '%' . $value . '%')
+                ->orWhere('group', 'ILIKE', '%' . $value . '%')
                 ;
         });
     }
