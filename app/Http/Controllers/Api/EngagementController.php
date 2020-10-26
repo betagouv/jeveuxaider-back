5d067cce45f1547917532b2ca60020fb9a6f1d8a
Ijahman <kevin.vaissaud@gmail.com>
2020-10-26 11:13:03 +0100

    correct warning composer 2.0

--- app/Http/Controllers/Api/EngagementController.php
+++ app/Http/Controllers/Api/EngagementController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
 use Algolia\AlgoliaSearch\SearchClient;
 use App\Http\Controllers\Controller;
--- app/Http/Controllers/Api/ParticipationController.php
+++ app/Http/Controllers/Api/ParticipationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
 use App\Exports\ParticipationsExport;
 use App\Filters\FiltersParticipationCollectivity;
