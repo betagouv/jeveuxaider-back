e5bb02a65d0a74a91facb0c565f1c53fdbcc4116
NVaissaud <n.vaissaud@gmail.com>
2020-12-07 11:16:32 +0100

    Message item menu mobile

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -382,6 +382,13 @@
                     >Mon compte</router-link
                   >
                   <router-link
+                    to="/messages"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                    @click.native="showMobileMenu = false"
+                    >Mes messages</router-link
+                  >
+                  <router-link
                     to="/logout"
                     class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     role="menuitem"
