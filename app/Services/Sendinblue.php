7f134a9391b04ea62275aa692dc7f817a0e78fb2
Jérémy Pinto <pinto.jeremy@gmail.com>
2021-02-02 15:32:29 +0100

    fix wrong phone number

--- app/Services/Sendinblue.php
+++ app/Services/Sendinblue.php
@@ -65,7 +65,7 @@ class Sendinblue
         $phoneNumberUtil = \libphonenumber\PhoneNumberUtil::getInstance();
         $mobile = $phoneNumberUtil->isPossibleNumber($user->profile->mobile, 'FR') ? $phoneNumberUtil->parse($user->profile->mobile, 'FR') : null;
 
-        if ($phoneNumberUtil->isPossibleNumber($user->profile->mobile, 'FR')) {
+        if (!$phoneNumberUtil->isPossibleNumber($user->profile->mobile, 'FR')) {
             dump("$user->email has a wrong phone number : " . $user->profile->mobile);
         }
         
