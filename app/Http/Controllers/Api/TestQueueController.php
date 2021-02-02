ca5c440b2ff1ed81adddd38ef29ff3eaef1b878f
Jérémy Pinto <pinto.jeremy@gmail.com>
2021-02-02 15:27:11 +0100

    fix bug parse phone sendinblue

--- app/Http/Controllers/Api/TestQueueController.php
+++ app/Http/Controllers/Api/TestQueueController.php
@@ -13,9 +13,9 @@ class TestQueueController extends Controller
     public function test($text)
     {
         // $phone = PhoneNumber::make('0671902872', 'FR')->isOfCountry('FR');
-        $user = User::where('email', 'Patrick.bonfils@jscs.gouv.fr')->first();
-        ray($user);
-        Sendinblue::sync($user);
+        $user = User::where('email', 'ines.he@live.fr')->first();
+
+        return Sendinblue::formatAttributes($user);
         return 'coucou';
         // TestJob::dispatch($text);
         // return $text;
