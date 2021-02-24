d4402171b38c6d3271257bbe32a5acf8506d71e4
Jérémy Pinto <pinto.jeremy@gmail.com>
2021-02-24 12:27:20 +0100

    nuxt analyze to false

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -69,7 +69,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    analyze: true,
+    analyze: false,
     extractCSS: process.env.NODE_ENV === 'production',
   },
 
