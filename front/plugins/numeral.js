21bcca94a10b1e4b32efd06b6916444a0327895d
Jérémy Pinto <pinto.jeremy@gmail.com>
2021-03-04 11:03:04 +0100

    disable numeral

--- front/plugins/numeral.js
+++ front/plugins/numeral.js
@@ -2,27 +2,27 @@ import Vue from 'vue'
 import numeral from 'numeral'
 
 // load a locale
-numeral.register('locale', 'fr', {
-  delimiters: {
-    thousands: ' ',
-    decimal: ',',
-  },
-  abbreviations: {
-    thousand: 'k',
-    million: 'm',
-    billion: 'b',
-    trillion: 't',
-  },
-  ordinal(number) {
-    return number === 1 ? 'er' : 'ème'
-  },
-  currency: {
-    symbol: '€',
-  },
-})
+// numeral.register('locale', 'fr', {
+//   delimiters: {
+//     thousands: ' ',
+//     decimal: ',',
+//   },
+//   abbreviations: {
+//     thousand: 'k',
+//     million: 'm',
+//     billion: 'b',
+//     trillion: 't',
+//   },
+//   ordinal(number) {
+//     return number === 1 ? 'er' : 'ème'
+//   },
+//   currency: {
+//     symbol: '€',
+//   },
+// })
 
-// switch between locales
-numeral.locale('fr')
+// // switch between locales
+// numeral.locale('fr')
 
 Vue.filter('formatNumber', function (number) {
   return numeral(number).format()
