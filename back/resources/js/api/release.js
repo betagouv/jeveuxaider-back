d8cbbdb4fa78d217d6ab064cf6da8cb44b7b4a8a
Jérémy Pinto <pinto.jeremy@gmail.com>
2021-03-11 15:39:33 +0100

    Remove back

--- back/resources/js/api/release.js
+++
@@ -1,5 +0,0 @@
-import request from '../utils/request'
-
-export function fetchReleases() {
-  return request.get('/api/releases')
-}
--- back/resources/js/plugins/nl2br.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import Nl2br from 'vue-nl2br'
-
-Vue.component('nl2br', Nl2br)
