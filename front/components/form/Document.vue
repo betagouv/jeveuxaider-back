f10c3d0a857631cbb84f4dc6c4caab373925b6e0
NVaissaud <n.vaissaud@gmail.com>
2021-03-15 11:28:52 +0100

    Icons document

--- front/components/form/Document.vue
+++ front/components/form/Document.vue
@@ -71,17 +71,16 @@
         </div>
       </div>
       <div v-else class="flex items-center">
-        <div
-          class="mr-4 rounded-full bg-gray-200 text-gray-700 flex items-center justify-center"
-          style="width: 50px; height: 50px"
-        >
-          <!-- <font-awesome-icon
-            :icon="[
-              'fas',
-              $options.filters.icoFromMimeType(form.file.mime_type),
-            ]"
-            class="text-xl"
-          /> -->
+        <div class="mr-4">
+          <img
+            :src="
+              require(`@/assets/images/dynamic/${$options.filters.icoFromMimeType(
+                form.file.mime_type
+              )}.svg`)
+            "
+            alt="File"
+            class="h-10 w-auto"
+          />
         </div>
         <div class="mr-8">
           <div>{{ form.file.file_name }}</div>
