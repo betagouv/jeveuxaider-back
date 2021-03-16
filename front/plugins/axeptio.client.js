fdc99a07c1767465f678caaca94b099f14859bcb
Jeremy <jeremy@codeconut.fr>
2021-03-16 16:57:26 +0100

    defer load scripts

--- front/plugins/axeptio.client.js
+++ front/plugins/axeptio.client.js
@@ -1,11 +1,13 @@
 export default () => {
-  const el = document.createElement('script')
-  el.setAttribute('src', 'https://static.axept.io/sdk.js')
-  el.setAttribute('type', 'text/javascript')
-  el.setAttribute('async', true)
-  el.setAttribute('data-id', '5fc77c519875a6702af31184')
-  el.setAttribute('data-cookies-version', 'ga_fb')
-  if (document.body !== null) {
-    document.body.appendChild(el)
-  }
+  document.addEventListener('DOMContentLoaded', function () {
+    const el = document.createElement('script')
+    el.setAttribute('src', 'https://static.axept.io/sdk.js')
+    el.setAttribute('type', 'text/javascript')
+    el.setAttribute('async', true)
+    el.setAttribute('data-id', '5fc77c519875a6702af31184')
+    el.setAttribute('data-cookies-version', 'ga_fb')
+    if (document.body !== null) {
+      document.body.appendChild(el)
+    }
+  })
 }
