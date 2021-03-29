4f6c658b58d7392f0bdfbcf4d20373f938483869
NVaissaud <n.vaissaud@gmail.com>
2021-03-29 16:36:03 +0200

    Fix newsletter subscription homepage

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1112,7 +1112,7 @@ export default {
       const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       if (re.test(String(email).toLowerCase())) {
         this.$axios
-          .post('/api/sendinblue/contact', { email })
+          .post('/sendinblue/contact', { email })
           .then(() => {
             this.successNewsletter = true
           })
