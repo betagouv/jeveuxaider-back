c0928e04d98ca4bd55a9f692115146474a7201f3
Ijahman <kevin.vaissaud@gmail.com>
2021-04-08 15:34:56 +0200

    plausible tracker without import

--- front/plugins/plausible.client.js
+++ front/plugins/plausible.client.js
@@ -1,13 +1,29 @@
-import Plausible from 'plausible-tracker'
+// import Plausible from 'plausible-tracker'
 
-export default (context, inject) => {
-  const options = {
-    domain: 'jeveuxaider.gouv.fr',
-    hashMode: false,
-    trackLocalhost: false,
-    apiHost: 'https://plausible.io',
-  }
-  const plausible = Plausible(options)
-  plausible.enableAutoPageviews()
-  inject('plausible', plausible)
+// export default (context, inject) => {
+//   const options = {
+//     domain: 'jeveuxaider.gouv.fr',
+//     hashMode: false,
+//     trackLocalhost: false,
+//     apiHost: 'https://plausible.io',
+//   }
+//   const plausible = Plausible(options)
+//   plausible.enableAutoPageviews()
+//   inject('plausible', plausible)
+// }
+
+export default () => {
+  document.addEventListener('DOMContentLoaded', function () {
+    // eslint-disable-next-line prettier/prettier
+    ; (function () {
+      const d = document
+      const s = d.createElement('script')
+
+      s.defer = 1
+      s.dataDomain = 'jeveuxaider.gouv.fr'
+      s.src = 'https://plausible.io/js/plausible.js'
+      s.async = 1
+      d.getElementsByTagName('head')[0].appendChild(s)
+    })()
+  })
 }
