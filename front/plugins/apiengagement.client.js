edb3f4071b2e6315e29adae7ae5442e58be6629c
NVaissaud <n.vaissaud@gmail.com>
2021-05-05 15:48:47 +0200

    Script apieng key hardcoded

--- front/plugins/apiengagement.client.js
+++ front/plugins/apiengagement.client.js
@@ -1,18 +1,18 @@
 /* eslint-disable */
-export default ({$config}) => {
-  if(!$config.apieng.key) {
+export default ({ $config }) => {
+  if (!$config.apieng.key) {
     return;
   }
   document.addEventListener('DOMContentLoaded', function () {
-    ;(function (i, s, o, g, r, a, m) {
+    ; (function (i, s, o, g, r, a, m) {
       i.ApiEngagementObject = r
-      ;(i[r] =
-        i[r] ||
-        function () {
-          ;(i[r].q = i[r].q || []).push(arguments)
-        }),
-        (i[r].l = 1 * new Date())
-      ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
+        ; (i[r] =
+          i[r] ||
+          function () {
+            ; (i[r].q = i[r].q || []).push(arguments)
+          }),
+          (i[r].l = 1 * new Date())
+        ; (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
       a.async = 1
       a.src = g
       m.parentNode.insertBefore(a, m)
@@ -23,6 +23,6 @@ export default ({$config}) => {
       'https://app.api-engagement.beta.gouv.fr/jstag.js',
       'apieng'
     )
-    apieng('config', $config.apieng.key)
+    '5f5931496c7ea514150a818f'
   })
 }
