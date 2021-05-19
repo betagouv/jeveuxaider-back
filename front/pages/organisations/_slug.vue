278fbbe3f94987b675e40aab9ad124c6332e53fa
Ijahman <kevin.vaissaud@gmail.com>
2021-05-19 15:45:26 +0200

    page orga: raccourci form edit pour admin

--- front/pages/organisations/_slug.vue
+++ front/pages/organisations/_slug.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <nuxt-link
+      v-if="$store.getters.contextRole === 'admin'"
+      :to="`/dashboard/structure/${organisation.id}/edit`"
+      class="fixed bottom-0 p-2 z-50 bg-white rounded-full m-4 shadow-lg hover:shadow-2xl border"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+        /></svg
+    ></nuxt-link>
+
     <!-- ROW 1 -->
     <div class="relative bg-white md:grid md:grid-cols-3 lg:grid-cols-2">
       <!-- 1 -- LEFT -->
