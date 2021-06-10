59dd5e7095e30794763eaa4cbbe98ab6aecf0822
Ijahman <kevin.vaissaud@gmail.com>
2021-06-10 16:45:39 +0200

    visibilité page mission depuis le dashboard

--- front/pages/dashboard/mission/_id/index.vue
+++ front/pages/dashboard/mission/_id/index.vue
@@ -3,11 +3,42 @@
     <div class="header px-12 flex">
       <div class="header-titles flex-1">
         <div class="text-m text-gray-600 uppercase">Mission</div>
-        <div class="flex flex-wrap mb-8 max-w-3xl">
+        <div class="mb-8 max-w-3xl">
           <div class="font-bold text-2-5xl text-gray-800 mr-2">
             {{ mission.name }}
           </div>
-          <TagModelState :state="mission.state" />
+          <div
+            v-if="
+              !['En attente de validation', 'Signalée'].includes(mission.state)
+            "
+            class="mt-2 flex items-center"
+          >
+            <div class="mr-2 text-gray-450">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <nuxt-link
+              target="_blank"
+              :to="`/missions-benevolat/${mission.id}/${mission.slug}`"
+            >
+              <span class="text-sm underline hover:no-underline">
+                {{ $config.appUrl }}/missions-benevolat/{{ mission.id }}/{{
+                  mission.slug
+                }}
+              </span>
+            </nuxt-link>
+          </div>
+          <TagModelState class="mt-4" :state="mission.state" />
         </div>
       </div>
       <div>
