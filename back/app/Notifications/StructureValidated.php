a3f323b69efe4c3c33cda2897f5762c76f3a7136
NVaissaud <n.vaissaud@gmail.com>
2021-06-29 17:12:16 +0200

    Recettes territoires

--- back/app/Notifications/StructureValidated.php
+++ back/app/Notifications/StructureValidated.php
@@ -52,7 +52,7 @@ class StructureValidated extends Notification
             ->greeting('Bonjour ' . $notifiable->first_name . ',')
             ->line('Votre organisation « ' . $this->structure->name . ' » vient d\'être validée.')
             ->line('Vous pouvez désormais publier directement vos missions depuis votre espace organisation.')
-            ->action('Mes missions', url(config('app.url').'/dashboard/missions'));
+            ->action('Mes missions', url(config('app.url') . '/dashboard/structure/' . $this->structure->id . '/missions'));
     }
 
     /**
