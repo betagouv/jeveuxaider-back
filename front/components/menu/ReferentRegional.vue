6a93f4c36786b217bf22d0dea2e4a54bb6268a0d
Ijahman <kevin.vaissaud@gmail.com>
2021-06-30 15:16:56 +0200

    Bug affichage lien actif quand on est sur la fiche d'un utilisateur

--- front/components/menu/ReferentRegional.vue
+++ front/components/menu/ReferentRegional.vue
@@ -102,7 +102,7 @@
       to="/dashboard/profiles"
       class="group flex items-center px-2 py-2 text-sm rounded-md"
       :class="[
-        doesPathContains('dashboard/profiles')
+        doesPathContains('dashboard/profile')
           ? 'bg-primary text-white font-semibold'
           : 'text-gray-700 hover:text-gray-900 hover:bg-gray-50 font-medium',
       ]"
