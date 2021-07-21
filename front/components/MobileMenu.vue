216b5dac4e17a43c9a15721d812ca4d90d5ef060
Jeremy <jeremy@codeconut.fr>
2021-07-21 10:21:30 +0200

    edit link

--- front/components/MobileMenu.vue
+++ front/components/MobileMenu.vue
@@ -71,7 +71,7 @@
           </button>
           <nuxt-link
             v-if="!$store.getters.isLogged"
-            to="/register/responsable"
+            to="/inscription/organisation"
             class="px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
             role="menuitem"
             @click.native="closeMobileMenu"
