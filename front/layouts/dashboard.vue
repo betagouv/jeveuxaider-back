9df4bbbb1cbca305f4dde22e42561f8eb056d459
NVaissaud <n.vaissaud@gmail.com>
2021-07-21 16:36:07 +0200

    Fix color table row

--- front/layouts/dashboard.vue
+++ front/layouts/dashboard.vue
@@ -13,15 +13,7 @@
 
         <portal-target
           v-if="$store.getters['volet/active'] && $store.getters['volet/row']"
-          class="
-            p-4
-            overflow-y-auto
-            flex-none
-            border-l
-            bg-gray-100
-            shadow-lg
-            z-10
-          "
+          class="p-4 overflow-y-auto flex-none border-l bg-gray-50"
           name="volet"
           style="width: 420px"
         />
