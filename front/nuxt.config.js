b28b7ca46732e2d510369702de22614d9d58970d
Jeremy <jeremy@codeconut.fr>
2021-07-30 15:12:39 +0200

    cache for static files

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -248,6 +248,11 @@ export default {
         },
       },
     },
+    // Setting up cache for 'static' directory - a year in milliseconds
+    // https://web.dev/uses-long-cache-ttl
+    static: {
+      maxAge: 60 * 60 * 24 * 365 * 1000,
+    },
   },
 
   sitemap: () => {
