0b0f926a5b0c73da8725b0f4b4e23dfa940fd44f
NVaissaud <n.vaissaud@gmail.com>
2021-09-29 15:30:51 +0200

    Update context role superviseur to tete_de_reseau

--- back/app/Console/Commands/CreateReseauxFromOrgaReseau.php
+++ back/app/Console/Commands/CreateReseauxFromOrgaReseau.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Profile;
 use App\Models\Reseau;
 use App\Models\Structure;
+use App\Models\User;
 use Illuminate\Console\Command;
 
 
@@ -41,7 +42,7 @@ class CreateReseauxFromOrgaReseau extends Command
      */
     public function handle()
     {
-        ray()->newScreen();
+        // ray()->newScreen();
 
         if ($this->confirm('Do you wish to continue?')) {
             // Clean field reseau_id
@@ -51,18 +52,23 @@ class CreateReseauxFromOrgaReseau extends Command
                 $reseau = Reseau::updateOrCreate([
                     'name' => $structure->name
                 ],[
-                    'created_at' => $structure->created_at, 
+                    'created_at' => $structure->created_at,
                     'updated_at' => $structure->updated_at
                 ]);
                 // Link antennes
-                $this->info("Réseau " . $reseau->name . " crée");
+                $this->info("Réseau " . $reseau->name . " créé ou updatée");
                 $antennesIds = Structure::where('reseau_id', $structure->id)->pluck("id")->toArray();
                 array_push($antennesIds, $structure->id);
                 $this->info("Réseau " . $reseau->name . " a " . count($antennesIds) . " antennes");
                 $reseau->structures()->syncWithoutDetaching($antennesIds);
                 // Link profile to reseau
                 Profile::where('reseau_id', $structure->id)->update(['tete_de_reseau_id' => $reseau->id]);
+                
             });
+
+            // Update context_role in users table
+            User::where('context_role','superviseur')->update(['context_role' => 'tete_de_reseau']);
+
         }
     }
 }
