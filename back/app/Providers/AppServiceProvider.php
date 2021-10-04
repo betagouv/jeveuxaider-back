ecaa381e749da9f3f6a2bd841e84d3e5e53571eb
Ijahman <kevin.vaissaud@gmail.com>
2021-10-04 15:09:32 +0200

    Quand un témoignage est créé, supprimer la notification témoignage correspondante

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -27,8 +27,10 @@ use App\Observers\MessageObserver;
 use Algolia\AlgoliaSearch\Config\SearchConfig;
 use Algolia\AlgoliaSearch\SearchClient;
 use App\Models\NotificationTemoignage;
+use App\Models\Temoignage;
 use App\Models\Territoire;
 use App\Observers\NotificationTemoignageObserver;
+use App\Observers\TemoignageObserver;
 use App\Observers\TerritoireObserver;
 
 class AppServiceProvider extends ServiceProvider
@@ -73,6 +75,7 @@ class AppServiceProvider extends ServiceProvider
         Invitation::observe(InvitationObserver::class);
         Territoire::observe(TerritoireObserver::class);
         NotificationTemoignage::observe(NotificationTemoignageObserver::class);
+        Temoignage::observe(TemoignageObserver::class);
 
 
         Validator::extend('phone', function ($attribute, $value, $parameters) {
