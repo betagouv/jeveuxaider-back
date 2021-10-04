fce636925868f9b1a4c07e77bfcd21d4b78580d7
Ijahman <kevin.vaissaud@gmail.com>
2021-10-04 11:01:59 +0200

    blindage

--- front/components/form/Mission.vue
+++ front/components/form/Mission.vue
@@ -625,16 +625,18 @@ export default {
           '([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?'
         ).test(value)
       ) {
-        callback(new Error(`Il n'est pas possible de rentrer un lien.`))
+        callback(new Error(`Les liens ne sont pas autorisés dans ce champ.`))
       } else {
         callback()
       }
     }
     const noPhoneInContent = (rule, value, callback) => {
       const regex = /(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})/
-      if (value.match(regex)) {
+      if (value?.match(regex)) {
         callback(
-          new Error(`Il n'est pas possible de rentrer un numéro de téléphone.`)
+          new Error(
+            `Les numéros de téléphones ne sont pas autorisés dans ce champ.`
+          )
         )
       } else {
         callback()
