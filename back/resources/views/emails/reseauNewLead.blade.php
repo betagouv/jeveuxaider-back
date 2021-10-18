94b18c6535a2066b983394f0ffbd27ce0e48bfb3
NVaissaud <n.vaissaud@gmail.com>
2021-10-18 12:47:33 +0200

    Edit lead reseau

--- back/resources/views/emails/reseauNewLead.blade.php
+++ back/resources/views/emails/reseauNewLead.blade.php
@@ -4,14 +4,21 @@ Bonjour,
 <p>Vous avez reçu un nouveau lead "Tête de réseau" :</p>
 
 @component('mail::table')
-| Libellé       | Valeur        |
+| Libellé | Valeur |
 | ------------- |:-------------:|
-| Organisation  | {{ $form['name'] }} |
-| Prénom        | {{ $form['first_name'] }} |
-| Nom           | {{ $form['last_name'] }} |
-| E-mail        | {{ $form['email'] }} |
-| Téléphone     | {{ $form['phone'] }} |
+| Organisation | {{ $form['name'] }} |
+| Nombre d'antennes | {{ $form['nb_antennes'] }} |
+| Nombre d'employés | {{ $form['nb_employees'] }} |
+| Nombre de bénévoles | {{ $form['nb_volunteers'] }} |
+| Prénom | {{ $form['first_name'] }} |
+| Nom | {{ $form['last_name'] }} |
+| E-mail | {{ $form['email'] }} |
+| Téléphone | {{ $form['phone'] }} |
 @endcomponent
+
+@if ($form['description'])
+<i>{{ $form['description'] }}</i>
+@endif
 
 Bonne journée,
 
