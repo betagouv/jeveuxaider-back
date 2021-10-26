f2f25d040439434e606ec1d9e554f9a75873ee59
Ijahman <kevin.vaissaud@gmail.com>
2021-10-26 16:24:19 +0200

    homepage : auto pause video when not in viewport

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,63 +1,7 @@
 <template>
   <div>
     <!-- BANNER -->
-    <section class="relative h-[590px]">
-      <picture class="lg:hidden">
-        <source
-          srcset="/images/banner.jpg, /images/banner@2x.jpg 2x"
-          media="(min-width: 440px)"
-        />
-        <img
-          srcset="/images/banner_mobile.jpg, /images/banner_mobile@2x.jpg 2x"
-          alt="Récolte de nourriture et de vêtement"
-          class="object-cover absolute w-full h-full"
-        />
-      </picture>
-
-      <video
-        autoplay
-        loop
-        muted
-        class="object-cover absolute w-full h-full hidden lg:block"
-      >
-        <source src="/video/banner.mp4" type="video/mp4" />
-      </video>
-
-      <div class="bg-black opacity-50 absolute inset-0 h-full w-full"></div>
-
-      <div class="relative z-10 h-full">
-        <div
-          class="container xl:max-w-[1412px] mx-auto px-4 text-center text-white h-full"
-        >
-          <div
-            class="flex flex-col justify-center items-center h-full text-shadow"
-          >
-            <div
-              class="text-[50px] leading-none lg:text-7xl font-extrabold mb-4 tracking-tighter"
-            >
-              Envie de bénévolat&nbsp;?
-            </div>
-
-            <h1 class="text-2xl lg:text-[28px] lg:leading-[34px] mb-8">
-              <strong>Devenez bénévole</strong>
-              <span>et trouvez des missions de bénévolat</span>
-              <br class="hidden md:block" />
-              <strong>près de chez vous</strong>
-              <span>ou</span>
-              <strong>à distance</strong>
-            </h1>
-
-            <button
-              class="flex items-center justify-center rounded-full text-white bg-jva-green hover:scale-105 !outline-none focus:scale-105 transition px-8 py-4 transform will-change-transform shadow-xl font-bold text-xl"
-              @click="handleClickCTA()"
-            >
-              <SearchSvg width="16" height="16" class="flex-none" />
-              <span class="ml-2">Trouver une mission</span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
+    <HomeBanner />
 
     <!-- ORGANISATIONS -->
     <section class="relative overflow-hidden md:h-[500px] md:flex items-center">
@@ -605,12 +549,7 @@
 </template>
 
 <script>
-import SearchSvg from '@/static/images/search.svg?inline'
-
 export default {
-  components: {
-    SearchSvg,
-  },
   data() {
     return {
       prioritizedMissions: [],
