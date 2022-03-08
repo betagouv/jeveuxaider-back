eb44f217419e2d1bf684d6f37ca5ecb717716730
Ijahman <kevin.vaissaud@gmail.com>
2022-03-08 15:56:19 +0100

    thottle

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // 'throttle:120,1',
+            'throttle:120,1',
             'bindings',
             \App\Http\Middleware\LastOnlineAt::class
         ],
