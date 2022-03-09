7f1393eaa95133fb9f185c7608f4a155dc0f0b8e
NVaissaud <n.vaissaud@gmail.com>
2022-03-09 17:16:17 +0100

    Migrate organisations missions from interface

--- app/Http/Controllers/Api/ScriptController.php
+++ app/Http/Controllers/Api/ScriptController.php
@@ -4,16 +4,49 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-
+use App\Http\Requests\Api\ScriptMigrateOrganisationMissionsRequest;
+use App\Models\Structure;
+use Illuminate\Support\Facades\Artisan;
 
 class ScriptController extends Controller
 {
-    public function migrateOrganisationMissions(Request $request)
+    public function migrateOrganisationMissions(ScriptMigrateOrganisationMissionsRequest $request)
     {
-        abort(422, "Vous n'avez pas les droits nécéssaires");
 
-        //
+        $structureOrigin = Structure::withCount(['missions'])->find($request->input('origin')['id']);
+        $structureDestination = Structure::find($request->input('destination')['id']);
+        $missionsToMigrate = $request->input('missions');
+
+        if(!$structureOrigin){
+            abort(422, "L'organisation d'origine n'existe plus");
+        }
+
+        if(!$structureOrigin->missions_count){
+            abort(422, "L'organisation d'origine n'a pas de mission");
+        }
+
+        if(!$structureDestination){
+            abort(422, "L'organisation de destination n'existe plus");
+        }
+
+        if($structureDestination->id == $structureOrigin->id){
+            abort(422, "Merci de sélectionner différentes organisations ! :)");
+        }
+
+        if($missionsToMigrate){
+            Artisan::call('migrate-organisation-missions', [
+                'id' => collect($missionsToMigrate)->pluck('id')->toArray(),
+                '--origin' => $structureOrigin->id,
+                '--destination' => $structureDestination->id,
+                '--no-interaction' => true
+            ]);
+        } else {
+            Artisan::call('migrate-organisation-missions', [
+                '--origin' => $structureOrigin->id,
+                '--destination' => $structureDestination->id,
+                '--no-interaction' => true
+            ]);
+        }
+
     }
 }
