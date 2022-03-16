9f82fc6c2c667c7dceb4ced67d52a01fed663bcd
NVaissaud <n.vaissaud@gmail.com>
2022-03-16 13:28:18 +0100

    Snu service try catch

--- app/Services/Snu.php
+++ app/Services/Snu.php
@@ -2,17 +2,24 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 
 class Snu
 {
     public function getWaitingActionsFromEmail($email)
     {
-        $response = Http::get(config('app.snu_api_url') . '/jeveuxaider/actions', [
-            'email' => $email,
-            'token' => config('app.snu_api_token')
-        ]);
+        try {
+            $response = Http::get(config('app.snu_api_url') . '/jeveuxaider/actions', [
+                'email' => $email,
+                'token' => config('app.snu_api_token')
+            ])->throw();
 
-        return isset($response['data']) ? $response['data']['actions'] : null;
+            return isset($response['data']) ? $response['data']['actions'] : null;
+        } catch (ConnectionException $e) {
+            throw $e;
+        }
     }
 }
