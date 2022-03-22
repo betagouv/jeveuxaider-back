1e0ce8507d58b9b2a2b89374fab7a8d32b9565e1
NVaissaud <n.vaissaud@gmail.com>
2022-03-22 12:59:28 +0100

    Add responsables in endpoint EngagementController organisations

--- app/Http/Controllers/Api/EngagementController.php
+++ app/Http/Controllers/Api/EngagementController.php
@@ -132,7 +132,7 @@ class EngagementController extends Controller
         }
 
         $results = QueryBuilder::for(Structure::where('state', 'Validée'))
-            ->with(['reseaux:id,name','domaines:id,name'])
+            ->with(['members', 'reseaux:id,name','domaines:id,name'])
             ->allowedFilters([
                 AllowedFilter::exact('department'),
                 'state',
@@ -142,42 +142,17 @@ class EngagementController extends Controller
             ->defaultSort('-id')
             ->paginate($request->input('pagination') ?? config('query-builder.results_per_page'));
 
-        $results->getCollection()->transform(function($item, $key) {
+        $results->getCollection()->transform(function($organisation, $key) {
             return [
-                'id' => $item->id,
-                'rna' => $item->rna,
-                'api_id' => $item->api_id,
-                'name' => $item->name,
-                'phone' => $item->phone,
-                'email' => $item->email,
-                'url' => $item->full_url,
-                'description' => $item->description,
-                'statut_juridique' => $item->statut_juridique,
-                'association_types' => $item->association_types,
-                'structure_publique_type' => $item->structure_publique_type,
-                'structure_publique_etat_type' => $item->structure_publique_etat_type,
-                'structure_privee_type' => $item->structure_privee_type,
-                'address' => [
-                    'full' => $item->full_address,
-                    'address' => $item->address,
-                    'zip' => $item->zip,
-                    'city' => $item->city,
-                    'department' => $item->department,
-                    'country' => $item->country,
-                    'latitude' => $item->latitude,
-                    'longitude' => $item->longitude,
-                ],
-                'website' => $item->website,
-                'facebook' => $item->facebook,
-                'twitter' => $item->twitter,
-                'instagram' => $item->instagram,
-                'donation' => $item->donation,
-                'created_at' => $item->created_at,
-                'publics_beneficiaires' => $item->publics_beneficiaires,
-                'domaines' => $item->domaines ? $item->domaines->map(function($domaine){
-                    return $domaine['name'];
+                ...$organisation->toSearchableArray(),
+                'responsables' => $organisation->has('members') ? $organisation->members->map(function($responsable){
+                    return [
+                        'id' => $responsable->id,
+                        'first_name' => $responsable->first_name,
+                        'last_name' => $responsable->last_name,
+                        'email' => $responsable->email,
+                    ];
                 })->all() : null,
-                'reseaux' => $item->reseaux ? $item->reseaux->all() : null,
             ];
         });
 
