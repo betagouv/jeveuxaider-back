6d68b9006f502ce4c8364889957976d34bd866c1
Ijahman <kevin.vaissaud@gmail.com>
2022-03-22 10:55:01 +0100

    correction override image 1 structure sur la page mission

--- app/Http/Controllers/Api/MissionController.php
+++ app/Http/Controllers/Api/MissionController.php
@@ -84,7 +84,7 @@ class MissionController extends Controller
     {
 
         if (is_numeric($id)) {
-            $mission = Mission::with(['structure.members:id,first_name,last_name,mobile,email', 'template.domaine', 'domaine', 'domaineSecondary', 'responsable', 'skills', 'template.photo', 'illustrations', 'structure.illustrations'])->withCount('temoignages')->where('id', $id)->first();
+            $mission = Mission::with(['structure.members:id,first_name,last_name,mobile,email', 'template.domaine', 'domaine', 'domaineSecondary', 'responsable', 'skills', 'template.photo', 'illustrations', 'structure.illustrations', 'structure.overrideImage1'])->withCount('temoignages')->where('id', $id)->first();
             if ($mission) {
                 $mission->append(['full_address', 'has_places_left']);
             }
