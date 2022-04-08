47fc1c187f407237c953db3e6901b5cd1f481792
Jérémy Pinto <pintojeremy@gmail.com>
2022-04-08 15:55:56 +0200

    Airtable sync

---
+++ app/Console/Commands/AirtableSyncMissions.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Mission;
+use App\Services\Airtable;
+use Illuminate\Console\Command;
+
+class AirtableSyncMissions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'airtable:sync-missions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync missions in Airtable';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $query = Mission::with(['structure', 'domaine', 'template.domaine']);
+
+        if ($this->confirm($query->count() . ' missions will be added or updated in Airtable')) {
+            $start = now();
+            $time = $start->diffInSeconds(now());
+
+            $this->comment("Processed in $time seconds");
+
+            $query->chunk(50, function ($missions) use ($start) {
+                foreach ($missions as $mission) {
+                    Airtable::syncMission($mission);
+                }
+                $time = $start->diffInSeconds(now());
+                $this->comment("Processed in $time seconds");
+            });
+        }
+    }
+}
