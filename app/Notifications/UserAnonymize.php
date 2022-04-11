fa46087e7b83aa4648655c22697bafdeba5332e7
Ijahman <kevin.vaissaud@gmail.com>
2022-04-11 19:00:29 +0200

    Email anonymisation compte utilisateur, wording email inscription bénévole

---
+++ app/Notifications/UserAnonymize.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserAnonymize extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function viaQueues()
+    {
+        return [
+            'mail' => 'emails',
+        ];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Suppression de votre compte sur JeVeuxAider.gouv.fr')
+            ->greeting('Bonjour,')
+            ->line('Votre compte sur JeVeuxAider.gouv.fr a bien été supprimé.')
+            ->line('Cette action est irréversible : pour utiliser la plateforme à nouveau, vous devrez recréer un compte.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
