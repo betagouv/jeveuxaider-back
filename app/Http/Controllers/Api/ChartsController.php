0be0bb76f2e7bdead2ecf319e129f680c4f9ef4f
NVaissaud <n.vaissaud@gmail.com>
2022-05-25 10:57:59 +0200

    Chart optimisations with date_trunc

--- app/Http/Controllers/Api/ChartsController.php
+++ app/Http/Controllers/Api/ChartsController.php
@@ -4,15 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Activity;
-use App\Models\Mission;
-use App\Models\MissionTemplate;
 use App\Models\Participation;
-use App\Models\Profile;
-use App\Models\Reseau;
-use Carbon\Carbon;
-use App\Models\Structure;
-use App\Models\Territoire;
 use Illuminate\Support\Facades\DB;
 
 class ChartsController extends Controller
@@ -20,28 +12,41 @@ class ChartsController extends Controller
 
     public $startDate;
     public $endDate;
+    public $department;
 
     public function __construct(Request $request)
     {
         $this->startYear = 2020;
         $this->endYear = date('Y');
+
+        if ($request->has('department')) {
+            $this->department = $request->input('department');
+        }
     }
 
     public function organisationsByDate(Request $request)
     {
         $items = [];
 
-        // SELECT date_trunc('month', "public"."profiles"."created_at") AS "created_at", count(*) AS "count"
-        // FROM "public"."profiles"
-        // GROUP BY date_trunc('month', "public"."profiles"."created_at")
-        // ORDER BY date_trunc('month', "public"."profiles"."created_at") ASC
+        $results = DB::select("
+                SELECT date_trunc('month', structures.created_at) AS created_at,
+                date_part('year', structures.created_at) as year,
+                date_part('month', structures.created_at) as month,
+                count(*) AS count
+                FROM structures
+                WHERE structures.department ILIKE :department
+                GROUP BY date_trunc('month', structures.created_at), year, month
+                ORDER BY date_trunc('month', structures.created_at) ASC
+            ", [
+            "department" => $this->department ? '%'.$this->department.'%' : '%%',
+        ]);
+
+        $collection = collect($results);
 
         for ($year = $this->startYear; $year <= $this->endYear; $year++) {
             for ($month = 1; $month < 13; $month++) {
-                $items[$year][]  = Structure::role($request->header('Context-Role'))
-                    ->whereYear('created_at', '=', $year)
-                    ->whereMonth('created_at', '=', $month)
-                    ->count();
+                $item = $collection->where('year', $year)->where('month', $month)->first();
+                $items[$year][] = $item ? $item->count : 0;
             }
         }
 
@@ -52,12 +57,25 @@ class ChartsController extends Controller
     {
         $items = [];
 
+        $results = DB::select("
+                SELECT date_trunc('month', missions.created_at) AS created_at,
+                date_part('year', missions.created_at) as year,
+                date_part('month', missions.created_at) as month,
+                count(*) AS count
+                FROM missions
+                WHERE missions.department ILIKE :department
+                GROUP BY date_trunc('month', missions.created_at), year, month
+                ORDER BY date_trunc('month', missions.created_at) ASC
+            ", [
+            "department" => $this->department ? '%'.$this->department.'%' : '%%',
+        ]);
+
+        $collection = collect($results);
+
         for ($year = $this->startYear; $year <= $this->endYear; $year++) {
             for ($month = 1; $month < 13; $month++) {
-                $items[$year][]  = Mission::role($request->header('Context-Role'))
-                    ->whereYear('created_at', '=', $year)
-                    ->whereMonth('created_at', '=', $month)
-                    ->count();
+                $item = $collection->where('year', $year)->where('month', $month)->first();
+                $items[$year][] = $item ? $item->count : 0;
             }
         }
 
@@ -68,12 +86,26 @@ class ChartsController extends Controller
     {
         $items = [];
 
+        $results = DB::select("
+                SELECT date_trunc('month', participations.created_at) AS created_at,
+                date_part('year', participations.created_at) as year,
+                date_part('month', participations.created_at) as month,
+                count(*) AS count
+                FROM participations
+                LEFT JOIN missions ON missions.id = participations.mission_id
+                WHERE missions.department ILIKE :department
+                GROUP BY date_trunc('month', participations.created_at), year, month
+                ORDER BY date_trunc('month', participations.created_at) ASC
+            ", [
+            "department" => $this->department ? '%'.$this->department.'%' : '%%',
+        ]);
+
+        $collection = collect($results);
+
         for ($year = $this->startYear; $year <= $this->endYear; $year++) {
             for ($month = 1; $month < 13; $month++) {
-                $items[$year][]  = Participation::role($request->header('Context-Role'))
-                    ->whereYear('created_at', '=', $year)
-                    ->whereMonth('created_at', '=', $month)
-                    ->count();
+                $item = $collection->where('year', $year)->where('month', $month)->first();
+                $items[$year][] = $item ? $item->count : 0;
             }
         }
 
@@ -84,12 +116,25 @@ class ChartsController extends Controller
     {
         $items = [];
 
+        $results = DB::select("
+                SELECT date_trunc('month', profiles.created_at) AS created_at,
+                date_part('year', profiles.created_at) as year,
+                date_part('month', profiles.created_at) as month,
+                count(*) AS count
+                FROM profiles
+                WHERE profiles.department ILIKE :department
+                GROUP BY date_trunc('month', profiles.created_at), year, month
+                ORDER BY date_trunc('month', profiles.created_at) ASC
+            ", [
+            "department" => $this->department ? $this->department.'%' : '%%',
+        ]);
+
+        $collection = collect($results);
+
         for ($year = $this->startYear; $year <= $this->endYear; $year++) {
             for ($month = 1; $month < 13; $month++) {
-                $items[$year][]  = Profile::role($request->header('Context-Role'))
-                    ->whereYear('created_at', '=', $year)
-                    ->whereMonth('created_at', '=', $month)
-                    ->count();
+                $item = $collection->where('year', $year)->where('month', $month)->first();
+                $items[$year][] = $item ? $item->count : 0;
             }
         }
 
