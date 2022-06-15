b1d44041919d63bcafd5e06ab2267c2e04dc495e
NVaissaud <n.vaissaud@gmail.com>
2022-06-15 12:44:48 +0200

    Filter exact on profile search + export

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Filters\FiltersParticipationSearch;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Profile;
@@ -12,8 +11,6 @@ use App\Filters\FiltersProfileSearch;
 use App\Filters\FiltersProfileRole;
 use App\Filters\FiltersProfileMinParticipations;
 use App\Http\Requests\ProfileRequest;
-use App\Models\Participation;
-use App\Models\Tag;
 use App\Sorts\ProfileParticipationsValidatedCountSort;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\AllowedSort;
@@ -32,8 +29,8 @@ class ProfileController extends Controller
                 AllowedFilter::custom('search', new FiltersProfileSearch),
                 AllowedFilter::custom('role', new FiltersProfileRole),
                 'department',
-                'referent_department',
-                'referent_region',
+                AllowedFilter::exact('referent_department'),
+                AllowedFilter::exact('referent_region'),
                 'zip',
                 AllowedFilter::exact('is_visible'),
                 AllowedFilter::custom('min_participations', new FiltersProfileMinParticipations)
@@ -45,62 +42,6 @@ class ProfileController extends Controller
             ])
             ->paginate(config('query-builder.results_per_page'));
     }
-
-    // public function statistics(Request $request, Profile $profile)
-    // {
-    //     return [
-    //         'participations' => [
-    //             'Toutes' => Participation::where('profile_id', $profile->id)->count(),
-    //             'En attente de validation' => Participation::where('profile_id', $profile->id)->where('state', 'En attente de validation')->count(),
-    //             'En cours de traitement' => Participation::where('profile_id', $profile->id)->where('state', 'En cours de traitement')->count(),
-    //             'Validée' => Participation::where('profile_id', $profile->id)->where('state', 'Validée')->count(),
-    //             'Refusée' => Participation::where('profile_id', $profile->id)->where('state', 'Refusée')->count(),
-    //             'Annulée' => Participation::where('profile_id', $profile->id)->where('state', 'Annulée')->count(),
-    //         ]
-    //     ];
-    // }
-
-    // public function export(Request $request)
-    // {
-
-    //     $currentUser = User::find(Auth::guard('api')->user()->id);
-
-    //     if(!$currentUser->profile->can_export_profiles){
-    //         return response()->json(['message'=> 'Seuls les référents accrédités peuvent générer cet export'], 401);
-    //     }
-
-    //     $folder = 'public/'. config('app.env').'/exports/'.$request->user()->id . '/';
-    //     $fileName = 'profiles-' . Str::random(8) . '.csv';
-    //     $filePath = $folder . $fileName;
-
-    //     (new ProfilesExport($request->header('Context-Role')))
-    //         ->queue($filePath, 's3')
-    //         ->chain([
-    //             new NotifyUserOfCompletedExport($request->user(), $filePath),
-    //         ]);
-
-    //     return response()->json(['message'=> 'Export en cours...'], 200);
-    // }
-
-    // public function exportReferentsDepartements(Request $request)
-    // {
-    //     return Excel::download(new ProfilesReferentsDepartementsExport(), 'referents-departements.csv', \Maatwebsite\Excel\Excel::CSV);
-    // }
-
-    // public function exportReferentsRegions(Request $request)
-    // {
-    //     return Excel::download(new ProfilesReferentsRegionsExport(), 'referents-regions.csv', \Maatwebsite\Excel\Excel::CSV);
-    // }
-
-    // public function exportTetesDeReseau(Request $request)
-    // {
-    //     return Excel::download(new ProfilesTetesDeReseauExport(), 'tetes-de-reseau.csv', \Maatwebsite\Excel\Excel::CSV);
-    // }
-
-    // public function exportResponsables(Request $request)
-    // {
-    //     return Excel::download(new ProfilesResponsablesExport(), 'responsables.csv', \Maatwebsite\Excel\Excel::CSV);
-    // }
 
     public function show(ProfileRequest $request, Profile $profile)
     {
@@ -129,56 +70,6 @@ class ProfileController extends Controller
 
         return $profile;
     }
-
-    // public function upload(ProfileUpdateRequest $request, Profile $profile)
-    // {
-
-    //     // Delete previous file
-    //     if ($media = $profile->getFirstMedia('profiles')) {
-    //         $media->delete();
-    //     }
-
-    //     $data = $request->all();
-    //     $extension = $request->file('image')->guessExtension();
-    //     $name = Str::random(30);
-
-    //     $cropSettings = json_decode($data['cropSettings']);
-    //     if (!empty($cropSettings)) {
-    //         $stringCropSettings = implode(",", [
-    //             $cropSettings->width,
-    //             $cropSettings->height,
-    //             $cropSettings->x,
-    //             $cropSettings->y
-    //         ]);
-    //     } else {
-    //         $pathName = $request->file('image')->getPathname();
-    //         $infos = getimagesize($pathName);
-    //         $stringCropSettings = implode(",", [
-    //             $infos[0],
-    //             $infos[1],
-    //             0,
-    //             0
-    //         ]);
-    //     }
-
-    //     $profile
-    //         ->addMedia($request->file('image'))
-    //         ->usingName($name)
-    //         ->usingFileName($name . '.' . $extension)
-    //         ->withManipulations([
-    //             'thumb' => ['manualCrop' => $stringCropSettings]
-    //         ])
-    //         ->toMediaCollection('profiles');
-
-    //     return $profile;
-    // }
-
-    // public function uploadDelete(ProfileUpdateRequest $request, Profile $profile)
-    // {
-    //     if ($media = $profile->getFirstMedia('profiles')) {
-    //         $media->delete();
-    //     }
-    // }
 
     public function firstname(Request $request)
     {
