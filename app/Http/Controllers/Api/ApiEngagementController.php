40ba60e824251a1628ed35db52d268615080169d
NVaissaud <n.vaissaud@gmail.com>
2022-09-14 15:58:37 +0200

    ApiEngagementController

--- app/Http/Controllers/Api/ApiEngagementController.php
+++ app/Http/Controllers/Api/ApiEngagementController.php
@@ -6,9 +6,45 @@ use App\Http\Controllers\Controller;
 use App\Services\ApiEngagement;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Carbon\Carbon;
 
 class ApiEngagementController extends Controller
 {
+    public $year;
+    public $month;
+    public $startDate;
+    public $endDate;
+    public $department;
+
+    public function __construct(Request $request)
+    {
+        if ($request->input('period') == 'current_year') {
+            $this->startDate = Carbon::now()->startOfYear()->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->endOfYear()->format('Y-m-d H:i:s');
+        } elseif ($request->input('period') == 'last_year') {
+            $this->startDate = Carbon::now()->subYear(1)->startOfYear()->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->subYear(1)->endOfYear()->format('Y-m-d H:i:s');
+        } elseif ($request->input('period') == 'current_month') {
+            $this->startDate = Carbon::now()->startOfMonth()->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->endOfMonth()->format('Y-m-d H:i:s');
+        } elseif ($request->input('period') == 'last_month') {
+            $this->startDate = Carbon::now()->subMonth(1)->startOfMonth()->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->subMonth(1)->endOfMonth()->format('Y-m-d H:i:s');
+        } elseif ($request->input('period') == 'current_week') {
+            $this->startDate = Carbon::now()->startOfWeek()->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->endOfWeek()->format('Y-m-d H:i:s');
+        } elseif ($request->input('period') == 'last_week') {
+            $this->startDate = Carbon::now()->subWeek(1)->startOfWeek()->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->subWeek(1)->endOfWeek()->format('Y-m-d H:i:s');
+        } else {
+            $this->startDate = Carbon::create(2000, 01, 01, 0, 0, 0)->format('Y-m-d H:i:s');
+            $this->endDate = Carbon::now()->format('Y-m-d H:i:s');
+        }
+
+        if ($request->has('department')) {
+            $this->department = $request->input('department');
+        }
+    }
 
     public function myMission(Request $request, Int $id)
     {
@@ -20,31 +56,13 @@ class ApiEngagementController extends Controller
         return $mission;
     }
 
-    public function statisticsMissions(Request $request)
-    {
-
-        $service = new ApiEngagement();
-
-        $statistics = $service->getStatistics([
-            'facets' => 'fromPublisherName,source',
-            'createdAt' => 'gt:2022-04-25',
-            //'createdAt' => 'lt:2022-05-25',
-            'toPublisherId' => '5f5931496c7ea514150a818f',
-            'type' => 'click',
-        ]);
-
-        ray($statistics);
-
-        return $statistics;
-    }
-
     public function outgoingTrafic(Request $request)
     {
 
         $service = new ApiEngagement();
 
-        $from = '2022-01-01';
-        $to = '2022-09-01';
+        $from = $this->startDate;
+        $to = $this->endDate;
 
         $results = $service->getStatistics("facets=fromPublisherName,source,toPublisherName&createdAt=gt:$from&createdAt=lt:$to&fromPublisherId=5f5931496c7ea514150a818f&type=click");
 
@@ -64,8 +82,8 @@ class ApiEngagementController extends Controller
 
         $service = new ApiEngagement();
 
-        $from = '2022-01-01';
-        $to = '2022-09-01';
+        $from = $this->startDate;
+        $to = $this->endDate;
 
         $results = $service->getStatistics("facets=fromPublisherName,source,toPublisherName&createdAt=gt:$from&createdAt=lt:$to&toPublisherId=5f5931496c7ea514150a818f&type=click");
 
@@ -85,8 +103,8 @@ class ApiEngagementController extends Controller
 
         $service = new ApiEngagement();
 
-        $from = '2022-01-01';
-        $to = '2022-09-01';
+        $from = $this->startDate;
+        $to = $this->endDate;
 
         $results = $service->getStatistics("facets=fromPublisherName,source,toPublisherName&createdAt=gt:$from&createdAt=lt:$to&fromPublisherId=5f5931496c7ea514150a818f&type=apply");
 
@@ -106,8 +124,8 @@ class ApiEngagementController extends Controller
 
         $service = new ApiEngagement();
 
-        $from = '2022-01-01';
-        $to = '2022-09-01';
+        $from = $this->startDate;
+        $to = $this->endDate;
 
         $results =  $service->getStatistics("facets=fromPublisherName,source,toPublisherName&createdAt=gt:$from&createdAt=lt:$to&toPublisherId=5f5931496c7ea514150a818f&type=apply");
 
