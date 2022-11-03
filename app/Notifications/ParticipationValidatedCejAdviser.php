2d77de316656f8b3de11fc8f84a35e0eee5ede5d
IJahman <kevin.vaissaud@gmail.com>
2022-11-03 13:22:13 +0100

    correction décalage liste dans emails

--- app/Notifications/ParticipationValidatedCejAdviser.php
+++ app/Notifications/ParticipationValidatedCejAdviser.php
@@ -57,13 +57,13 @@ class ParticipationValidatedCejAdviser extends Notification implements ShouldQue
     public function toMail($notifiable)
     {
         $message = (new MailMessage)
-            ->subject($this->participation->profile->full_name.' s’est inscrit sur une mission de bénévolat')
+            ->subject($this->participation->profile->full_name . ' s’est inscrit sur une mission de bénévolat')
             ->greeting('Bonjour,')
-            ->line(new HtmlString($this->participation->profile->full_name.', que vous accompagnez dans le cadre du Contrat d’Engagement Jeune, s’est inscrit sur la mission <a href="'.url(config('app.front_url').$this->participation->mission->full_url).'">'.$this->participation->mission->name.'</a>.'))
+            ->line(new HtmlString($this->participation->profile->full_name . ', que vous accompagnez dans le cadre du Contrat d’Engagement Jeune, s’est inscrit sur la mission <a href="' . url(config('app.front_url') . $this->participation->mission->full_url) . '">' . $this->participation->mission->name . '</a>.'))
             ->line('En cas de besoin, vous pouvez consulter les ressources mises à votre disposition :')
             ->line(new HtmlString('<ul><li><a href="https://www.jeveuxaider.gouv.fr/engagement/wp-content/uploads/GUIDE-CEJ-Mise-a-jour-Septembre-2022.pdf">👉 Le Guide</a></li>'))
-            ->line(new HtmlString('<ul><li><a href="https://www.jeveuxaider.gouv.fr/engagement/wp-content/uploads/Presentation-CEJ-A4-Synthetique-Mise-a-jour-Septembre-2022.pdf">👉 La Fiche récapitulative</a></li>'))
-            ->line(new HtmlString('<ul><li><a href="https://vimeo.com/750348414">👉 La vidéo de présentation de JeVeuxAider.gouv.fr</a></li></ul>'))
+            ->line(new HtmlString('<li><a href="https://www.jeveuxaider.gouv.fr/engagement/wp-content/uploads/Presentation-CEJ-A4-Synthetique-Mise-a-jour-Septembre-2022.pdf">👉 La Fiche récapitulative</a></li>'))
+            ->line(new HtmlString('<li><a href="https://vimeo.com/750348414">👉 La vidéo de présentation de JeVeuxAider.gouv.fr</a></li></ul>'))
             ->line('Pour toute question, vous pouvez également contacter notre équipe par retour de mail.');
 
         return $message;
--- app/Notifications/RegisterUserVolontaireCejAdviser.php
+++ app/Notifications/RegisterUserVolontaireCejAdviser.php
@@ -52,15 +52,15 @@ class RegisterUserVolontaireCejAdviser extends Notification implements ShouldQue
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject($this->profile->full_name.' s’est inscrit sur JeVeuxAider.gouv.fr')
+            ->subject($this->profile->full_name . ' s’est inscrit sur JeVeuxAider.gouv.fr')
             ->greeting('Bonjour,')
-            ->line(new HtmlString($this->profile->full_name.', que vous accompagnez dans le cadre du Contrat d’Engagement Jeune, a rejoint la plateforme <a href="'.url(config('app.front_url')).'">JeVeuxAider.gouv.fr</a>.'))
-            ->line(new HtmlString('<a href="'.url(config('app.front_url')).'">JeVeuxAider.gouv.fr</a> est la <a href="'.url(config('app.front_url')).'">plateforme publique du bénévolat</a>, proposée par la Réserve civique. Elle met en relation celles et ceux qui veulent agir pour l’intérêt général avec les associations, établissements publics et communes qui ont besoin de bénévoles.'))
+            ->line(new HtmlString($this->profile->full_name . ', que vous accompagnez dans le cadre du Contrat d’Engagement Jeune, a rejoint la plateforme <a href="' . url(config('app.front_url')) . '">JeVeuxAider.gouv.fr</a>.'))
+            ->line(new HtmlString('<a href="' . url(config('app.front_url')) . '">JeVeuxAider.gouv.fr</a> est la <a href="' . url(config('app.front_url')) . '">plateforme publique du bénévolat</a>, proposée par la Réserve civique. Elle met en relation celles et ceux qui veulent agir pour l’intérêt général avec les associations, établissements publics et communes qui ont besoin de bénévoles.'))
             ->line('Afin que vous puissiez l’accompagner dans son parcours en tant que bénévole, vous serez notifié par mail à son inscription sur une mission de bénévolat.')
             ->line('En cas de besoin, vous pouvez consulter les ressources mises à votre disposition :')
             ->line(new HtmlString('<ul><li><a href="https://www.jeveuxaider.gouv.fr/engagement/wp-content/uploads/GUIDE-CEJ-Mise-a-jour-Septembre-2022.pdf">👉 Le Guide</a></li>'))
-            ->line(new HtmlString('<ul><li><a href="https://www.jeveuxaider.gouv.fr/engagement/wp-content/uploads/Presentation-CEJ-A4-Synthetique-Mise-a-jour-Septembre-2022.pdf">👉 La Fiche récapitulative</a></li>'))
-            ->line(new HtmlString('<ul><li><a href="https://vimeo.com/750348414">👉 La vidéo de présentation de JeVeuxAider.gouv.fr</a></li></ul>'))
+            ->line(new HtmlString('<li><a href="https://www.jeveuxaider.gouv.fr/engagement/wp-content/uploads/Presentation-CEJ-A4-Synthetique-Mise-a-jour-Septembre-2022.pdf">👉 La Fiche récapitulative</a></li>'))
+            ->line(new HtmlString('<li><a href="https://vimeo.com/750348414">👉 La vidéo de présentation de JeVeuxAider.gouv.fr</a></li></ul>'))
             ->line('Pour toute question, vous pouvez également contacter notre équipe par retour de mail.');
     }
 
