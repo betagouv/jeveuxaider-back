e811138229df440f5d25b4723aca42db404fde4f
Jérémy Pinto <pintojeremy@gmail.com>
2022-11-10 15:29:31 +0100

    blindage responsable mission

--- app/Http/Requests/MissionRequest.php
+++ app/Http/Requests/MissionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Structure;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -27,7 +28,13 @@ class MissionRequest extends FormRequest
         return [
             'user_id' => 'sometimes|required',
             'name' => 'required_without:template_id|min:3|max:255',
-            'responsable_id' => 'required',
+            'responsable_id' => ['required', function ($attribute, $value, $fail) {
+                $datas = $this->validator->getData();
+                $structure = Structure::with('members.profile')->find($datas['structure_id']);
+                if (! $structure->members->pluck('profile.id')->contains($value)) {
+                    $fail("Le responsable renseigné n'est pas un membre de l'organisation");
+                }
+            }],
             'start_date' => 'required|date',
             'end_date' => 'nullable|date|after_or_equal:start_date',
             'structure_id' => '',
