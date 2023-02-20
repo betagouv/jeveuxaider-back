9468ed74d5002081bc4921fe66d30f0b0e3e8336
IJahman <kevin.vaissaud@gmail.com>
2023-02-20 19:04:37 +0100

    correction banniere email en mobile

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -10,46 +10,27 @@
 <table cellpadding="0" cellspacing="0" border="0" width="100%"
     style="width: 100% !important; min-width: 100%; max-width: 100%;">
     <tr>
-        <td align="center" valign="bottom" height="161"
+        <td align="center" valign="bottom" height="110"
             background="{{ config('app.front_url') }}/images/mail/header.jpg"
-            style="width: 100%; background-image:url({{ config('app.front_url') }}/images/mail/header.jpg); background-repeat: no-repeat; background-position: top center;"
+            style="width: 100%; background-image:url({{ config('app.front_url') }}/images/mail/header.jpg); background-repeat: no-repeat; background-position: center; background-size: cover;"
             bgcolor="#f5f8fa">
             <!--[if (gte mso 9)|(IE)]>
-                <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:100%;height:161px;">
+                <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:100%;height:110;">
                 <v:fill type="tile" src="{{ config('app.front_url') }}/images/mail/header.jpg" color="#f5f8fa" />
                 <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
             <![endif]-->
             <!--[if (gte mso 9)|(IE)]>
-                <table border="0" cellspacing="0" cellpadding="0" width="100%" height="161">
-                <tr><td align="center" valign="bottom" width="100%" height="161">
+                <table border="0" cellspacing="0" cellpadding="0" width="100%" height="110">
+                <tr><td align="center" valign="bottom" width="100%" height="110">
             <![endif]-->
-            <table cellpadding="0" cellspacing="0" border="0" width="100%"
-                style="width: 100% !important; min-width: 100%; max-width: 100%;">
-                <tr>
-                    <td align="center" valign="top" height="17"
-                        style="height: 17px; max-height: 17px; min-height: 17px;">
-                        <div style="height: 17px; line-height: 17px; font-size: 15px">&nbsp;
-                        </div>
-                    </td>
-                </tr>
-            </table>
-            <table cellpadding="0" cellspacing="0" border="0" width="100%"
-                style="width: 100% !important; min-width: 100%; max-width: 100%;">
-                <tr>
-                    <td align="center" valign="top" height="8"
-                        style="height: 8px; max-height: 8px; min-height: 8px;">
-                        <div style="height: 8px; line-height: 8px; font-size: 6px">&nbsp;
-                        </div>
-                    </td>
-                </tr>
-            </table>
             <!--[if (gte mso 9)|(IE)]>
-                              </td></tr>
-                              </table><![endif]-->
+                </td></tr>
+                </table>
+            <![endif]-->
             <!--[if (gte mso 9)|(IE)]>
-                                 </v:textbox>
-                                 </v:rect>
-                              <![endif]-->
+                </v:textbox>
+                </v:rect>
+            <![endif]-->
         </td>
     </tr>
 </table>
