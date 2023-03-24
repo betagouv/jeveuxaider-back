fd74b9691b575d46ce24bed2c02bdd25db9e352c
IJahman <kevin.vaissaud@gmail.com>
2023-03-24 12:25:39 +0100

    correction export filtre public beneficiaire

--- app/Exports/MissionsExport.php
+++ app/Exports/MissionsExport.php
@@ -6,6 +6,7 @@ use App\Filters\FiltersMissionDate;
 use App\Filters\FiltersMissionIsTemplate;
 use App\Filters\FiltersMissionPlacesLeft;
 use App\Filters\FiltersMissionPublicsVolontaires;
+use App\Filters\FiltersMissionPublicsBeneficiaires;
 use App\Filters\FiltersMissionSearch;
 use App\Models\Mission;
 use Illuminate\Http\Request;
@@ -50,6 +51,7 @@ class MissionsExport implements FromQuery, WithMapping, WithHeadings
                 AllowedFilter::custom('place', new FiltersMissionPlacesLeft),
                 AllowedFilter::custom('date', new FiltersMissionDate),
                 AllowedFilter::custom('publics_volontaires', new FiltersMissionPublicsVolontaires),
+                AllowedFilter::custom('publics_beneficiaires', new FiltersMissionPublicsBeneficiaires),
                 AllowedFilter::custom('search', new FiltersMissionSearch),
                 AllowedFilter::scope('available'),
                 AllowedFilter::custom('is_template', new FiltersMissionIsTemplate),
