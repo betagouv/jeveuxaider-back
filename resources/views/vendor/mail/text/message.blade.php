5be5dba08ad7f5f4333f645da0be82a7be021d1e
NVaissaud <n.vaissaud@gmail.com>
2023-04-21 10:54:09 +0200

    Notification mail text message without url scalingo

--- resources/views/vendor/mail/text/message.blade.php
+++ resources/views/vendor/mail/text/message.blade.php
@@ -1,18 +1,1 @@
-@component('mail::layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-            {{ config('app.name') }}
-        @endcomponent
-    @endslot
-
-    {{-- Body --}}
-    {{ $slot }}
-
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
-        @endcomponent
-    @endslot
-@endcomponent
+{{ $slot }}
