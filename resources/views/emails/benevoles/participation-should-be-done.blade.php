4db729cbc67c7b257eaa6006771a2cfc62e99a3b
IJahman <kevin.vaissaud@gmail.com>
2023-05-24 16:02:49 +0200

    correction typos

--- resources/views/emails/benevoles/participation-should-be-done.blade.php
+++ resources/views/emails/benevoles/participation-should-be-done.blade.php
@@ -3,16 +3,16 @@
         {{ $benevole->first_name }}, avez-vous réalisé votre mission ? 🙌
     @endcomponent
     @component('mail::components.paragraph')
-        Il y a quelques temps, vous aviez proposé votre aide pour la mission <strong style="color: #1a1a1a; font-weight: 600;">{{ $mission->name }}</strong> de {{ $organisation->name }}.
+        Il y a quelques temps, vous avez proposé votre aide pour la mission <strong style="color: #1a1a1a; font-weight: 600;">{{ $mission->name }}</strong> de {{ $organisation->name }}.
     @endcomponent
     @component('mail::components.paragraph')
         On a besoin de savoir si vous avez participé à cette mission ou non.
     @endcomponent
     @component('mail::components.paragraph')
-        Rendez-vous sur votre espace pour mettre à jour le statut de votre participation. Vous pourrez la valider (💪🏻) ou l’annuler si elle n’est plus d’actualité (😢).
+        Rendez-vous sur votre espace pour mettre à jour le statut de votre participation. Vous pourrez la valider ou l’annuler si elle n’est plus d’actualité.
     @endcomponent
     @component('mail::components.paragraph')
-        Si la mission n'a pas encore ou lieu ou n'est pas encore terminée, nous vous invitons à le faire plus tard.
+        Si la mission n'a pas encore eu lieu ou n'est pas encore terminée, nous vous invitons à le faire plus tard.
     @endcomponent
     @component('mail::components.space', ['height' => 24])
     @endcomponent
