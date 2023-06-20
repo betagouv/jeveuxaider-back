5e66d6b85b193879a68c477d240f3e5d6700ebb9
NVaissaud <n.vaissaud@gmail.com>
2023-06-20 13:46:53 +0200

    FiltersConversationSearch

--- app/Filters/FiltersConversationSearch.php
+++ app/Filters/FiltersConversationSearch.php
@@ -4,18 +4,21 @@ namespace App\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
+use Illuminate\Support\Facades\Auth;
 
 class FiltersConversationSearch implements Filter
 {
     public function __invoke(Builder $query, $value, string $property): Builder
     {
 
-        return $query->whereHas('users', function (Builder $subquery) use ($value) {
+        return $query->whereHas('users', function (Builder $query) use ($value) {
             if (is_array($value)) {
                 $value = implode(',', $value);
             }
-            $subquery->whereHas('profile', function ($q) use ($value) {
-                $q->whereRaw("concat(first_name, ' ', last_name, ' ', email) ilike '%$value%' ");
+            $query->whereHas('profile', function (Builder $query) use ($value) {
+                $query
+                    ->whereRaw("concat(first_name, ' ', last_name, ' ', email) ilike '%$value%' ")
+                    ->where('user_id', '!=', Auth::guard('api')->user()->id);
             });
         });
     }
