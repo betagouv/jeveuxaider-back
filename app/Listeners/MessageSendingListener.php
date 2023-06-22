fa336235867e5007c30c4bbeb8e7bf47eb6f9b2e
IJahman <kevin.vaissaud@gmail.com>
2023-06-22 10:50:38 +0200

    blindage

--- app/Listeners/MessageSendingListener.php
+++ app/Listeners/MessageSendingListener.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\Models\User;
 use Illuminate\Mail\Events\MessageSending;
 use Illuminate\Support\Facades\App;
 
@@ -9,9 +10,11 @@ class MessageSendingListener
 {
     public function handle( MessageSending $event )
     {
-        $user = $event->data['notifiable'];
-        if ($user->anonymous_at) {
-            return false;
+        if (isset($event->data['notifiable']) && $event->data['notifiable'] instanceof User) {
+            $user = $event->data['notifiable'];
+            if ($user->anonymous_at) {
+                return false;
+            }
         }
 
         // @todo: si utilisateur est bloqué
