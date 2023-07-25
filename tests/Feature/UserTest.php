c003f3645b4567ef56614c75af4695f5e1443d17
Jérémy Pinto <pintojeremy@gmail.com>
2023-07-25 16:45:37 +0200

    test can register user

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,6 +1,32 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
-test('example', function () {
+use function Pest\Faker\fake;
 
+it('can register as benevole', function () {
+    $email = fake()->email;
+
+    $userData = [
+        'name' => $email,
+        'email' => $email,
+        'password' => Hash::make('password123'),
+        'first_name' => fake()->firstName,
+        'last_name' => fake()->lastName,
+        'mobile' => fake('fr_FR')->phoneNumber,
+        'birthday' => fake()->date(),
+        'zip' => fake('fr_FR')->postcode,
+        'country' => 'FR',
+    ];
+
+    $response = $this->post('/api/register/volontaire', $userData);
+
+    $response->assertStatus(201);
+
+    $user = User::latest()->first();
+
+    expect($user)
+        ->email->toBe($userData['email'])
+        ->context_role->toBe('volontaire');
 });
