12922e69b0157c93f7de535c648fdefc2b0ef5be
IJahman <kevin.vaissaud@gmail.com>
2024-02-28 12:06:23 +0100

    wording email participation will start

--- resources/views/emails/benevoles/participation-will-start.blade.php
+++ resources/views/emails/benevoles/participation-will-start.blade.php
@@ -5,14 +5,14 @@
     @component('mail::components.paragraph')
         L’organisation <strong style="color: #1a1a1a; font-weight: 600;">{{ $organisation->name }}</strong> compte sur vous pour honorer votre engagement !
     @endcomponent
-    @component('mail::components.paragraph', ['title' => 'Petit rappel de la mission'])
+    @component('mail::components.paragraph', ['title' => 'Votre mission'])
         <div>{{ $mission->name }}</div>
         @component('mail::components.space', ['height' => 10])
         @endcomponent
         <a class="link" href="{{ $url }}">Plus de détails ›</a>
     @endcomponent
     @component('mail::components.tips', ['title' => 'Vous avez des questions sur la mission ?'])
-        N’hésitez pas à contacter directement le responsable de l’association.
+        N’hésitez pas à contacter directement le responsable de l’organisation.
         @component('mail::components.space', ['height' => 24])
         @endcomponent
         @component('mail::button', ['url' => $url, 'align' => 'left'])
