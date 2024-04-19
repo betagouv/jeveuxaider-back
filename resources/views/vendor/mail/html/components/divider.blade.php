550343e367d059a358c71f2f188e8ec3fa6e35ed
IJahman <kevin.vaissaud@gmail.com>
2024-04-19 12:50:56 +0200

    Add divider mail component

---
+++ resources/views/vendor/mail/html/components/divider.blade.php
@@ -0,0 +1,17 @@
+<table cellpadding="0" cellspacing="0" border="0">
+    <tr>
+        <td height="{{$spaceTop ?? 24}}" bgcolor="{{$colorTop ?? '#FFFFFF'}}"></td>
+    </tr>
+</table>
+<table cellpadding="0" cellspacing="0" border="0" width="88%" style="width: 88% !important; min-width: 88%; max-width: 88%;">
+    <tr>
+        <td>
+            <hr style="border-color: #E5E5E5; background-color: #E5E5E5; color: #E5E5E5;">
+        </td>
+    </tr>
+</table>
+<table cellpadding="0" cellspacing="0" border="0">
+    <tr>
+        <td height="{{$spaceBottom ?? 24}}" bgcolor="{{$colorBottom ?? '#FFFFFF'}}"></td>
+    </tr>
+</table>
