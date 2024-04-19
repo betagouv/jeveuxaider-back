9071eb0101c8e83143e49c038e9f50f83667cd76
IJahman <kevin.vaissaud@gmail.com>
2024-04-19 12:51:55 +0200

    Optional title in tips mail component

--- resources/views/vendor/mail/html/components/tips.blade.php
+++ resources/views/vendor/mail/html/components/tips.blade.php
@@ -1,26 +1,28 @@
-@component('mail::components.space', ['height' => 36])
+@component('mail::components.space', ['height' => $spaceTop ?? 36])
 @endcomponent
-<table cellpadding="0" cellspacing="0" border="0" width="88%"
-    style="width: 88% !important; min-width: 88%; max-width: 88%; border-width: 1px; border-style: solid; border-color: #e8e8e8; border-bottom: none; border-left: none; border-right: none;">
-    <tbody>
-        <tr>
-            <td class="tips-wrapper" align="left" valign="top">
-                @component('mail::components.space', ['height' => 36])
-                @endcomponent
-                <div class="title">{{ $title ?? 'Astuces' }}</div>
-                @component('mail::components.space', ['height' => 14])
-                @endcomponent
-            </td>
-        </tr>
-    </tbody>
-</table>
+@isset($title)
+    <table cellpadding="0" cellspacing="0" border="0" width="88%"
+        style="width: 88% !important; min-width: 88%; max-width: 88%; border-width: 1px; border-style: solid; border-color: #e8e8e8; border-bottom: none; border-left: none; border-right: none;">
+        <tbody>
+            <tr>
+                <td class="tips-wrapper" align="left" valign="top">
+                    @component('mail::components.space', ['height' => 36])
+                    @endcomponent
+                    <div class="title">{{ $title }}</div>
+                    @component('mail::components.space', ['height' => 14])
+                    @endcomponent
+                </td>
+            </tr>
+        </tbody>
+    </table>
+@endisset
 <table cellpadding="0" cellspacing="0" border="0" width="88%"
     style="width: 88% !important; min-width: 88%; max-width: 88%; background: #070191; font-size: 1px; border-top-left-radius: 8px; border-top-right-radius: 8px;">
     <tbody>
         <tr>
             <td width="10" style="width: 10px; max-width: 10px; min-width: 10px;">&nbsp;</td>
             <td align="center" valign="top">
-                @component('mail::components.space', ['height' => 10, 'color' => '#070191'])
+                @component('mail::components.space', ['height' => 8, 'color' => '#070191'])
                 @endcomponent
             </td>
             <td width="10" style="width: 10px; max-width: 10px; min-width: 10px;">&nbsp;</td>
