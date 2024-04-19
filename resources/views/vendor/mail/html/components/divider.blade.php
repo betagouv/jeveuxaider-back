a929de1abd37d7129efd61e006abed5df3b9e327
IJahman <kevin.vaissaud@gmail.com>
2024-04-19 13:10:31 +0200

    Fix divider mail component in mail clients

--- resources/views/vendor/mail/html/components/divider.blade.php
+++ resources/views/vendor/mail/html/components/divider.blade.php
@@ -3,11 +3,9 @@
         <td height="{{$spaceTop ?? 24}}" bgcolor="{{$colorTop ?? '#FFFFFF'}}"></td>
     </tr>
 </table>
-<table cellpadding="0" cellspacing="0" border="0" width="88%" style="width: 88% !important; min-width: 88%; max-width: 88%;">
+<table cellpadding="0" cellspacing="0" border="0" width="88%" style="width: 88% !important; min-width: 88%; max-width: 88%; border-width: 1px; border-style: solid; border-color: #E5E5E5; border-bottom: none; border-left: none; border-right: none;">
     <tr>
-        <td>
-            <hr style="border-color: #E5E5E5; background-color: #E5E5E5; color: #E5E5E5;">
-        </td>
+        <td></td>
     </tr>
 </table>
 <table cellpadding="0" cellspacing="0" border="0">
