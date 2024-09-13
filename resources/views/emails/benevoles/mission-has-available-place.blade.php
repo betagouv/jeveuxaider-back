e8063cd714ff1b2ad903ebf8bbee7d04018a00ea
NVaissaud <n.vaissaud@gmail.com>
2024-09-13 15:10:52 +0200

    Modify wording mail

--- resources/views/emails/benevoles/mission-has-available-place.blade.php
+++ resources/views/emails/benevoles/mission-has-available-place.blade.php
@@ -1,24 +1,20 @@
 @component('mail::message')
     @component('mail::components.headline')
-        🙌 Ça y est ! Vous pouvez proposer votre aide pour la mission :
+        🙌 Bonjour {{ $notifiable->profile->first_name }},
     @endcomponent
     @component('mail::components.paragraph')
-        <a class="link" href="{{ $url }}">{{ $mission->name }}</a>
+        Ça y est ! Vous pouvez proposer votre aide pour la mission : <a class="link" href="{{ $url }}">{{ $mission->name }}</a>
+    @endcomponent
+    @component('mail::components.paragraph', ['title' => 'À vous de jouer !'])
+        Si la mission vous plaît toujours et que vous êtes disponible, proposez votre aide à l’organisation !
     @endcomponent
     @component('mail::button', ['url' => $url])
         Voir la mission
     @endcomponent
     @component('mail::components.space', ['height' => 32])
     @endcomponent
-    @component('mail::components.divider', ['spaceTop' => 32, 'spaceBottom' => 32])
-    @endcomponent
-    @component('mail::components.paragraph', ['title' => 'À vous de jouer !'])
-        Si la mission vous plaît toujours et que vous êtes disponible, proposez votre aide à l’organisation !
-    @endcomponent
-    @component('mail::components.divider', ['spaceTop' => 32, 'spaceBottom' => 32])
-    @endcomponent
-    @component('mail::components.tips')
-        <p>Ne l’oubliez pas, lorsque vous proposez votre aide sur une mission, les organisations comptent sur vous. Si vous ne pouvez plus y participer, il suffit de les prévenir à l’aide de la messagerie.</p>
+    @component('mail::components.tips', ['title' => 'Bonne pratique'])
+        <p>Ne l’oubliez pas, lorsque vous proposez votre aide sur une mission, les organisations comptent sur vous.</p>
         <p style="font-weight: 800;">Si vous ne pouvez plus y participer, il suffit de les prévenir à l’aide de la <a class="link" href="{{ $urlMessages }}">messagerie</a>.</p>
     @endcomponent
     @component('mail::components.divider', ['spaceTop' => 32, 'spaceBottom' => 32])
